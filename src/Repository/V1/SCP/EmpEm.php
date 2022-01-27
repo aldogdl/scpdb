39396ed2a466a4b2e081e0fb43f0a8752c01f662
Ing. Aldo Cervantes <aldo.gdl@hotmail.com>
2022-01-27 13:43:39 -0600

    FIX reparo el add de miembros 1

--- src/Repository/V1/SCP/EmpEm.php
+++ src/Repository/V1/SCP/EmpEm.php
@@ -54,9 +54,9 @@ class EmpEm extends RepoEm
                 $this->result['body'] = 'ERROR!! ' . $th->getMessage();
             }
         }else{
-            $this->result['abort'] = true;
+            $this->result['abort'] = false;
             $this->result['msg'] = 'has';
-            $this->result['body'] = 'Ya existe el Usuario';
+            $this->result['body'] = $hasUsername[0]->getId();
         }
         return $this->result;
     }
@@ -66,7 +66,19 @@ class EmpEm extends RepoEm
      */
     public function addProvBasicoEmp($data): array
     {
-        $obj = new UsEmpresa();
+        $obj = null;
+        if(array_key_exists('idEmp', $data)) {
+            if($data['idEmp'] != 0) {
+                $res = $this->getProveedorById($data['idEmp']);
+                if($res) {
+                    $obj = $res[0];
+                }
+            }
+        }
+
+        if($obj == null) {
+            $obj = new UsEmpresa();
+        }
         $obj->setNombre($data['emp']);
         $obj->setDespeq('Alta TMP desde SCP');
         $obj->setTipo($this->em->getPartialReference(UsEmpresaTipos::class, $data['tipo']));
@@ -90,8 +102,21 @@ class EmpEm extends RepoEm
      */
     public function addProvBasicoSuc($idEmp, $data): array
     {
-        $obj = new UsSucursales();
-        $obj->setEmpresa($this->em->getPartialReference(UsEmpresa::class, $idEmp));
+        $obj = null;
+        if(array_key_exists('idSuc', $data)) {
+            if($data['idSuc'] != 0) {
+                $res = $this->getSucursalById($data['idSuc']);
+                if($res) {
+                    $obj = $res[0];
+                }
+            }
+        }
+
+        if($obj == null) {
+            $obj = new UsSucursales();
+            $obj->setEmpresa($this->em->getPartialReference(UsEmpresa::class, $idEmp));
+        }
+        $obj->setDomicilio($data['dom']);
         $obj->setTelefono($data['tel']);
 
         try {
@@ -111,8 +136,19 @@ class EmpEm extends RepoEm
      */
     public function addProvBasicoContact($ids, $data): array
     {
-        $obj = new UsContacts();
-        $obj->setUser($this->em->getPartialReference(UsAdmin::class, $ids['idUser']));
+        $obj = null;
+        if(array_key_exists('id', $data)) {
+            if($data['id'] != 0) {
+                $res = $this->getContactoById($data['id']);
+                if($res) {
+                    $obj = $res[0];
+                }
+            }
+        }
+        if($obj == null) {
+            $obj = new UsContacts();
+            $obj->setUser($this->em->getPartialReference(UsAdmin::class, $ids['idUser']));
+        }
         $obj->setSucursal($this->em->getPartialReference(UsSucursales::class, $ids['suc']));
         $obj->setNombre($data['cta']);
         $obj->setCelular($data['cel']);
@@ -160,6 +196,25 @@ class EmpEm extends RepoEm
         'JOIN emp.tipo tipo '.
         'WHERE emp.tipo IN (:tipos)';
         return $this->em->createQuery($dql)->setParameter('tipos', [11,12,13]);
+    }
+    
+    /** */
+    public function getSucursalById($idSuc)
+    {
+        $dql = 'SELECT partial suc.{id, domicilio, telefono, empresa}, emp ' .
+        'FROM ' . UsSucursales::class . ' suc '.
+        'JOIN suc.empresa emp '.
+        'WHERE suc.id = :id';
+        return $this->em->createQuery($dql)->setParameter('id', $idSuc);
+    }
+
+    /** */
+    public function getContactoById($idCtc)
+    {
+        $dql = 'SELECT partial ct.{id, nombre, celular, cargo} ' .
+        'FROM ' . UsContacts::class . ' ct '.
+        'WHERE ct.id = :id';
+        return $this->em->createQuery($dql)->setParameter('id', $idCtc);
     }
 
     /** */
